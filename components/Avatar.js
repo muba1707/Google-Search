b3bc96ad1b9e7572813102c4d12819aed82b7e4b
muba@1707 <batcha.1707@gmail.com>
2022-03-12 17:55:18 +0530

    build HeaderOptions

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,10 +1,10 @@
 // import Image from "next/image";
 
-function Avatar({ url }) {
+function Avatar({ url, className }) {
   return (
     <img
       loading="lazy"
-      className="h-10 rounded-full cursor-pointer transition-duration-150 transform hover:scale-110"
+      className={`h-10 rounded-full cursor-pointer transition-duration-150 transform hover:scale-110 ${className}`}
       alt="profilepic"
       src={url}
     />
--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,8 @@ import { MicrophoneIcon, SearchIcon, XIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useRef } from "react";
+import Avatar from "./Avatar";
+import HeaderOptions from "../components/HeaderOptions";
 
 function Header() {
   const router = useRouter();
@@ -15,37 +17,42 @@ function Header() {
     router.push(`/search?term=${term}`);
   };
   return (
-    <header>
-      <Image
-        src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
-        height={40}
-        width={120}
-        className="cursor-pointer"
-        alt="google"
-        onClick={() => router.push("/")}
-      />
-      <form className="flex px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
-        <input
-          type="text"
-          ref={searchInputValue}
-          className="flex-grow w-full focus:outline-none"
+    <header className="sticky top-0 bg-white">
+      <div className="flex w-full p-6 items-center">
+        <Image
+          src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          height={40}
+          width={120}
+          className="cursor-pointer"
+          alt="google"
+          onClick={() => router.push("/")}
         />
-        <XIcon
-          className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
-          onClick={() => (searchInputValue.current.value = "")}
+        <form className="flex flex-grow px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
+          <input
+            type="text"
+            ref={searchInputValue}
+            className="flex-grow w-full focus:outline-none"
+          />
+          <XIcon
+            className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
+            onClick={() => (searchInputValue.current.value = "")}
+          />
+          <MicrophoneIcon className="mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300 cursor-pointer" />
+          <SearchIcon
+            className="h-6 text-blue-500 hidden sm:inline-flex cursor-pointer"
+            onClick={search}
+          />
+          <button hidden type="submit" onClick={search}>
+            Search
+          </button>
+        </form>
+        <Avatar
+          className="ml-auto"
+          url="https://avatars.githubusercontent.com/u/51027125?v=4"
         />
-        <MicrophoneIcon className="mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300 cursor-pointer" />
-        <SearchIcon className="h-6 text-blue-500 hidden sm:inline-flex cursor-pointer" />
-        <button
-          hidden
-          type="submit"
-          onClick={() => {
-            search;
-          }}
-        >
-          Search
-        </button>
-      </form>
+      </div>
+
+      <HeaderOptions />
     </header>
   );
 }
