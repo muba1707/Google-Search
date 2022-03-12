dca1958223ef01dd1648799afde4ad7af588cabe
muba@1707 <batcha.1707@gmail.com>
2022-03-12 21:06:40 +0530

    configure googleapi

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -12,11 +12,11 @@ function HeaderOptions() {
   return (
     <div className="flex w-full text-gray-700 justify-evenly text-sm lg:text-base lg:justify-start lg:space-x-36 lg:pl-52 border-b-[1px]">
       <div className="flex space-x-6">
-        <HeaderOption Icon={SearchIcon} title="All" />
+        <HeaderOption Icon={SearchIcon} title="All" selected />
         <HeaderOption Icon={PhotographIcon} title="Images" />
         <HeaderOption Icon={PlayIcon} title="Videos" />
         <HeaderOption Icon={NewspaperIcon} title="News" />
-        <HeaderOption Icon={MapIcon} title="Maps" selected />
+        <HeaderOption Icon={MapIcon} title="Maps" />
         <HeaderOption Icon={DotsVerticalIcon} title="More" />
       </div>
 
--- pages/search.js
+++ pages/search.js
@@ -1,16 +1,40 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import { useRouter } from "next/router";
 
-function Search() {
+const API_KEY = process.env.API_KEY;
+const CONTEXT_KEY = process.env.CONTEXT_KEY;
+
+function Search({ results }) {
+  const router = useRouter();
+
+  console.log(results);
   return (
     <div>
       <Head>
-        <title>Google Search</title>
+        <title>{router.query.term} - Google Search</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const startIndex = context.query.start || "0";
+
+  const data = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`
+  )
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
+
+  //After the server process the request it will send to the clientpass the result to the client
+  return {
+    props: {
+      results: data,
+    },
+  };
 }
 
 export default Search;
